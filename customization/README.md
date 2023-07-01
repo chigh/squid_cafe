301943e465c091d65a4a3a147baa05b67be27cd9
Clair High <chigh@squid.cafe>
2023-07-01 13:52:21 -0400

    updated .gitignore with duplicate files listings noted targaryen.sh with obsolete settings

--- customization/README.md
+++ customization/README.md
@@ -1,11 +1,7 @@
 # targaryen.sh 
 
 * Increases/changes the maximum post size.
-* Increases/changes the maximum user bio size.
-* Changes a dragon emoji.
 * I'm not a fan of "toot" (or "tweet" for that matter). Changes references to "toot" to "post".
-
-For my purposes, I have only changed the bio size from 160 to 300 chars.
 
 Originally obtained from: https://social.targaryen.house/@queen
 
--- customization/targaryen.sh
+++ customization/targaryen.sh
@@ -6,15 +6,19 @@
 #sed -i -e 's/500/800/g' \
     #app/javascript/mastodon/features/compose/components/compose_form.js \
     #app/validators/status_length_validator.rb \
-    #storybook/stories/character_counter.story.js \
-    #config/locales/*.yml
+    # obsolete #storybook/stories/character_counter.story.js \
+    # obsolete #config/locales/*.yml
 
-### Increase bio length
-cd live
-sed -i -e 's/160/300/g' \
-    app/javascript/packs/public.js \
-    app/models/account.rb \
-    app/views/settings/profiles/show.html.haml
+### Increase bio length -- obsolete
+# cd live
+#sed -i -e 's/160/300/g' \
+#    app/javascript/packs/public.js \
+#    app/models/account.rb \
+#    app/views/settings/profiles/show.html.haml
+
+### fields in bio set to 5 instead of 4 app/models/account.rb ~ +95
+# validates :fields, length: { maximum: 5 }, if: -> { local? && will_save_change_to_fields? }
+
 
 ### Dragon emoji
 #sed -i -e 's/1f602/1f432/g' \
